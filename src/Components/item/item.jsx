bde697b1ef58993858ea06ab52eff5c63a1c1e50
unknown <souravsharma7194@gmail.com>
2025-11-26 22:47:01 +0530

    did a little props drilling today

--- src/Components/item/item.jsx
+++ src/Components/item/item.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import './Item.css'
 
 const item = (props) => {
   return (
     <div className='item'>
 <img src={props.image} alt="" />
 <p>{props.name}</p>
+<div className='item-prices'>
+    <div className="item-price-new">
+        {props.new_price}
+    </div>
+    <div className="item-price-old">{props.new_old}
+
+    </div>
+</div>
     </div>
   )
 }
